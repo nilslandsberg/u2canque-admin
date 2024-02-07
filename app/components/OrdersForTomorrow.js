1d1b533c753aed1940f0da94741746f193d3e7a4
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-02-07 16:01:36 -0500

    <OrdersForTomorrow> changed to CSR.  useEffect implemented to handle data fetching.

--- app/components/OrdersForTomorrow.js
+++ app/components/OrdersForTomorrow.js
@@ -1,17 +1,40 @@
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from 'react';
 import { getOrdersForTomorrow } from '../utils/getOrdersForTomorrow';
 import RenderOrders from './RenderOrders';
 
-const OrdersForTomorrow = async () => {
-  const response = await getOrdersForTomorrow();
-  const orders = response.orders;
+const OrdersForTomorrow = () => {
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try{
+        const response = await getOrdersForTomorrow();
+        setOrders(response.orders);
+        setIsLoading(false)
+      } catch (error) {
+        console.error('Error fecthing orders: ', error)
+        setIsLoading(false)
+      }
+    };
+    
+    fetchData();
+  }, [])
+  
 
   return (
-    <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
-      <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
-        <RenderOrders orders={orders} />
+    <>
+    {isLoading ? <div className="text-white">Loading...</div> : 
+      <div className="flex overflow-x-scroll pb-10 hide-scroll-bar">
+        <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+          <RenderOrders orders={orders} />
+        </div>
       </div>
-    </div>
+    }
+    </>
+
   );
 }
 
