5fa640ddf14b26eb006385066c5457aaef284b7c
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-04-02 15:59:38 -0400

    character removed from return statement in app/page.js

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import OrdersForToday from "./components/OrdersForToday";
 
 export default async function Home() {
 
-  return ( q
+  return ( 
     <>
       <main>
         <div className="flex flex-col items-center justify-start bg-black pt-[160px]">
