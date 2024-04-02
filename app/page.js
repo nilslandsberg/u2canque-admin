24e50c6d95e5818e0b97ccbc7f273b5add7bccf7
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-04-02 15:58:20 -0400

    small edit to app/page.js

--- app/page.js
+++ app/page.js
@@ -1,9 +1,8 @@
-import { auth } from "./auth";
 import OrdersForToday from "./components/OrdersForToday";
 
 export default async function Home() {
 
-  return (
+  return ( q
     <>
       <main>
         <div className="flex flex-col items-center justify-start bg-black pt-[160px]">
