6af20fc6048327b30fcbe96da876eded85c11e72
nilslandsberg <111305349+nilslandsberg@users.noreply.github.com>
2024-05-15 11:03:10 -0400

    Links added for independence-day page.js.  Logic added for rendering IndependenceDayOrders.js

---
+++ app/components/IndependenceDayOrders.js
@@ -0,0 +1,61 @@
+"use client";
+
+import React, { useEffect, useState } from 'react';
+import RenderOrders from './RenderOrders';
+import { getIndependenceDayOrders } from '../utils/getMemorialDayOrders';
+
+const IndependenceDayOrders = () => {
+  const [orders, setOrders] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [date, setDate] = useState("");
+
+  const currentYear = new Date().getFullYear();
+  
+ 
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    let token = ""
+  
+    if (user) {
+      token = user.token;
+    }
+    
+    const fetchData = async () => {
+      try {
+        const response = await getIndependenceDayOrders(token);
+        setOrders(response.independenceDayOrders);
+        if (response.independenceDayOrders.length > 0) {
+          setDate(response.independenceDayOrders[0].pickUpDate); 
+        }
+        setIsLoading(false);
+      } catch (error) {
+        console.error('Error fetching orders: ', error);
+        setIsLoading(false);
+      }
+    };
+    
+    fetchData();
+  }, [])
+  
+  return (
+    <>
+      {isLoading ? <div className="text-white text-center">Loading...</div> : 
+        <>
+          { orders.length === 0 ? (
+            <div className="text-white text-center pt-4 text-2xl font-bold">There are no orders for Independence Day.</div>
+          ) : (
+            <>
+              <div className="text-white z-35 font-bold text-center text-xl">Orders For Independence Day {currentYear}</div>
+              <div className="flex flex-nowrap lg:ml-40 md:ml-20 ml-10 ">
+                <RenderOrders orders={orders} />
+              </div>
+            </>
+            )
+          }
+        </>
+      }
+    </>
+  );
+}
+
+export default IndependenceDayOrders;
--- app/components/NavBarLinks.js
+++ app/components/NavBarLinks.js
@@ -21,6 +21,7 @@ function NavBarLinks() {
             <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/thanksgiving">Thanksgiving</a>
             <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/christmas">Christmas</a>
             <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/memorial-day">Memorial Day</a>
+            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/independence-day">Independence Day</a>
           </div>
         </div>
         
