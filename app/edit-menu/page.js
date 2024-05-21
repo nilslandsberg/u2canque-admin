113c03860b43374186ff16e9cb83f7377240f30d
Johnny Wright <johnnywrightiv@gmail.com>
2024-05-20 19:31:09 -0500

    fix second scroll bar bug in edit-menu/page.js

--- app/edit-menu/page.js
+++ app/edit-menu/page.js
@@ -6,7 +6,8 @@ export default function page() {
     <>
       <main>
         <div className="flex flex-col items-center justify-center bg-black pt-[160px]">
-          <div className="max-w-screen-lg w-full h-screen overflow-auto">
+          <div className="max-w-screen-lg w-full overflow-auto">
+          {/* ^ removed h-screen class as it was causing second scroll bar */}
             <EditMenuOptions />
           </div>
         </div>
