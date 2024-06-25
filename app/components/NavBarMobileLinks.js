8f778a9d207a62bf42e2afcc59677ae50a47b805
Johnny Wright <95382311+Johnnywrightiv@users.noreply.github.com>
2024-06-25 17:03:45 -0500

    Update NavBarMobileLinks.js - move edit-menu to outer div

--- app/components/NavBarMobileLinks.js
+++ app/components/NavBarMobileLinks.js
@@ -15,19 +15,19 @@ function NavBarMobiledivs() {
           <a className="block py-2 px-4 text-sm hover:bg-slate-700" href="/next-business-day">Next Business Day</a>
           <a className="block py-2 px-4 text-sm hover:bg-slate-700" href="/next-week">Next Week</a>
           <div className="relative block py-2 px-4 group text-sm  transition-all">
-          <div className="flex cursor-pointer items-center gap-2  group-hover:text-gray-400">
-            <span>Holiday Orders</span>
-            <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
-          </div>
-          <div className="absolute left-0 top-10 hidden w-auto flex-col gap-1 rounded-lg bg-white py-3 shadow-md transition-all group-hover:flex text-black">
-            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/easter">Easter</a>
-            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/thanksgiving">Thanksgiving</a>
-            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/christmas">Christmas</a>
-            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/memorial-day">Memorial Day</a>
-            <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/independence-day">Independence Day</a>
+            <div className="flex cursor-pointer items-center gap-2  group-hover:text-gray-400">
+              <span>Holiday Orders</span>
+              <IoIosArrowDown className="rotate-180 transition-all group-hover:rotate-0" />
+            </div>
+            <div className="absolute left-0 top-10 hidden w-auto flex-col gap-1 rounded-lg bg-white py-3 shadow-md transition-all group-hover:flex text-black">
+              <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/easter">Easter</a>
+              <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/thanksgiving">Thanksgiving</a>
+              <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/christmas">Christmas</a>
+              <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/memorial-day">Memorial Day</a>
+              <a className="hover:text-gray-400 flex items-center py-1 pl-6 pr-8" href="/independence-day">Independence Day</a>
+            </div>
           </div>
           <a className="block py-2 px-4 text-sm hover:bg-slate-700" href="/edit-menu">Edit Menu</a>
-        </div>
         </div>
       )}
     </>
